5ba555069af3ccca9eacc7fe59c2be47cbfbbd76
muhammadfandu <muhammad.p.widodo@gmail.com>
2023-04-27 11:03:14 +0700

    integrate nextra framework

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+})
+ 
+module.exports = withNextra(nextConfig)
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>Documentation</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra',
+    },
+    // ...
+  }
