d1b801a8618953050afe08aa87c047af62c1b5b4
muhammadfandu <muhammad.p.widodo@gmail.com>
2023-09-04 21:03:01 +0700

    update imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from './theme-provider';
+import { ThemeProvider } from '../src/theme-provider';
 
 export default function App({ Component, pageProps }: AppProps) {
 
